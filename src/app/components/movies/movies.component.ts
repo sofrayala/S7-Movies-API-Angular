f26a3c5e8517ea0acc2923f3eadbb9c09c7dd939
Sofia Ayala <sofiarayala1995@gmail.com>
2025-05-20 20:25:27 +0200

    updated movies.html

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './movies.component.html',
-  styleUrl: './movies.component.css'
+  styleUrl: './movies.component.css',
 })
-export class MoviesComponent {
-
-}
+export class MoviesComponent {}
