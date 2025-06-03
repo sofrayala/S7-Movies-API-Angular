4651d84fe6a63b1c6af35141ac103369f57ce9eb
Sofia Ayala <sofiarayala1995@gmail.com>
2025-06-03 18:29:17 +0200

    updated Readme and env folder with example

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,19 @@
+// change this to your firebase config and rename this file to environment.ts
+
+export const environment = {
+  production: false,
+
+  tmbd: {
+    apiUrl: 'https://api.themoviedb.org/3/',
+    api_key: 'your api_key',
+  },
+
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+  },
+};
