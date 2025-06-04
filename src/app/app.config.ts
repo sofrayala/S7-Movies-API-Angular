1b1f21a4b752501a7c666ee130632b2aebf446bb
Sofia Ayala <sofiarayala1995@gmail.com>
2025-06-04 19:15:34 +0200

    improved css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,6 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
